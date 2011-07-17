35353fe00b9831e51a407221a39b6a0a77f5b7e0
Mark Cahill <mark@tiemonster.info>
2011-07-17 13:51:24 +0000

    Forgot to remove dev server

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ var app = express.createServer();
 app.use(express['static'](__dirname));
 
 // Enable for testing on c9
-app.listen(process.env.C9_PORT);
+//app.listen(process.env.C9_PORT);
 
 exports.app = app;
